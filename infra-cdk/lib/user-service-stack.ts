2ba6ff98c413b4e4a5b265a38d7e11313d52f128
minhkien4516 <kienmaitrungminh@gmail.com>
2025-10-27 23:15:57 +0700

    Adding lambda worker translation and sqs event source

--- infra-cdk/lib/user-service-stack.ts
+++ infra-cdk/lib/user-service-stack.ts
@@ -42,11 +42,11 @@ export class UserServiceStack extends cdk.Stack {
     );
 
     // Worker Lambda Function
-    const workerLambda = lambda.Function.fromFunctionArn(
-      this,
-      'TranslationWorkerLambda',
-      'arn:aws:lambda:ap-southeast-1:438465128644:function:UserServiceStack-WorkerLambdaBD11C0E2-6CEhvO6x65eO'
-    );
+    // const workerLambda = lambda.Function.fromFunctionArn(
+    //   this,
+    //   'TranslationWorkerLambda',
+    //   'arn:aws:lambda:ap-southeast-1:438465128644:function:UserServiceStack-WorkerLambdaBD11C0E2-6CEhvO6x65eO'
+    // );
 
     // API Gateway
     const api = apigateway.RestApi.fromRestApiId(
@@ -77,26 +77,26 @@ export class UserServiceStack extends cdk.Stack {
     );
 
     // worker lambda
-    // const workerLambda = new lambda.Function(this, 'WorkerLambda', {
-    //   runtime: lambda.Runtime.NODEJS_20_X,
-    //   handler: 'backend-app/src/worker/handler.handler',
-    //   code: lambda.Code.fromAsset('../backend-app/dist'),
-    //   environment: {
-    //     TRANSLATION_QUEUE_URL: translationQueue.queueUrl,
-    //     DYNAMODB_TABLE_NAME: translateTable.tableName,
-    //   },
-    //   timeout: cdk.Duration.seconds(60),
-    // });
+    const workerLambda = new lambda.Function(this, 'WorkerLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'backend-app/src/worker/handler.handler',
+      code: lambda.Code.fromAsset('../backend-app/dist'),
+      environment: {
+        TRANSLATION_QUEUE_URL: translationQueue.queueUrl,
+        DYNAMODB_TABLE_NAME: translateTable.tableName,
+      },
+      timeout: cdk.Duration.seconds(60),
+    });
 
-    // workerLambda.addEventSource(
-    //   new lambdaEventSources.SqsEventSource(translationQueue, {
-    //     batchSize: 5,
-    //     enabled: true,
-    //   })
-    // );
+    workerLambda.addEventSource(
+      new lambdaEventSources.SqsEventSource(translationQueue, {
+        batchSize: 5,
+        enabled: true,
+      })
+    );
 
-    // translationQueue.grantConsumeMessages(workerLambda);
-    // translateTable.grantReadWriteData(workerLambda);
+    translationQueue.grantConsumeMessages(workerLambda);
+    translateTable.grantReadWriteData(workerLambda);
 
     // Outputs
     new cdk.CfnOutput(this, 'LambdaFunctionArn', {
